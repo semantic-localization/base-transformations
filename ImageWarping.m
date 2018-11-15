89b2a0166d1f231a64e03f3c1e95a530dee1251f
Jayant Sharma <jayantsharma1990@gmail.com>
2018-11-15 13:30:15 -0600

    diff warping

--- ImageWarping.m
+++ ImageWarping.m
@@ -1,25 +1,10 @@
-function im_rectified = ImageWarping(im, H)
-%% Prep
+function im_warped = ImageWarping(im, H)
+
 im = double(im);
-% Refer calib_fisheye_zshade.txt
-fx = 562.89536;
-fy = 557.29656;
-px = 630.7712;
-py = 363.16152;
-omega = 1.03815;
-tan_omega_half_2 = 2 * tan(omega/2);
-
-
-%% PLAN : Original -> Undistort -> Homography -> Distort -> Final
-%% Since Ops are inverse => Ops : Distort -> inv(H) -> Undistort
-%% One go : meshgrid -> inv(H) -> distort -> interpolate
-
+H = inv(H);
 
 [u_x, u_y] = meshgrid(1:(size(im,2)), 1:(size(im,1)));
 h = size(u_x, 1); w = size(u_x,2);
-
-%% Step 1 : inverse Homo
-H = inv(H);
 
 v_x = H(1,1)*u_x + H(1,2)*u_y + H(1,3);
 v_y = H(2,1)*u_x + H(2,2)*u_y + H(2,3);
@@ -28,45 +13,9 @@ v_z = H(3,1)*u_x + H(3,2)*u_y + H(3,3);
 v_x = v_x./v_z;
 v_y = v_y./v_z;
 
+im_warped(:,:,1) = reshape(interp2(im(:,:,1), v_x(:), v_y(:)), [h, w]);
+im_warped(:,:,2) = reshape(interp2(im(:,:,2), v_x(:), v_y(:)), [h, w]);
+im_warped(:,:,3) = reshape(interp2(im(:,:,3), v_x(:), v_y(:)), [h, w]);
 
-%% Step 2: distort meshgrid
-u_n = (v_x - px) / fx;
-v_n = (v_y - py) / fy;
-
-r_u = sqrt(u_n.^2 + v_n.^2);
-r_d = 1/omega * atan(r_u * tan_omega_half_2);
-
-u_dn = r_d./r_u .* u_n;
-v_dn = r_d./r_u .* v_n;
-
-u_d = fx*u_dn + px;
-v_d = fy*v_dn + py;
-
-im_warped(:,:,1) = reshape(interp2(im(:,:,1), u_d(:), v_d(:)), [h, w]);
-im_warped(:,:,2) = reshape(interp2(im(:,:,2), u_d(:), v_d(:)), [h, w]);
-im_warped(:,:,3) = reshape(interp2(im(:,:,3), u_d(:), v_d(:)), [h, w]);
-
-im_rectified = uint8(im_warped);
-
-
-%% Step 3 : Distort (undistort meshgrid)
-% [u_x, u_y] = meshgrid(1:(size(im,2)), 1:(size(im,1)));
-% h = size(u_x, 1); w = size(u_x,2);
-
-% u_dn = (u_x - px) / fx;
-% v_dn = (u_y - py) / fy;
-
-% r_d = sqrt(u_dn .^ 2 + v_dn .^ 2);
-% r_u = tan(omega * r_d) / tan_omega_half_2;
-
-% u_n = (r_u ./ r_d) .* u_dn;
-% v_n = (r_u ./ r_d) .* v_dn;
-
-% v_x = u_n * fx + px;
-% v_y = v_n * fy + py;
-
-% im_final(:,:,1) = reshape(interp2(im_warped(:,:,1), v_x(:), v_y(:)), [h, w]);
-% im_final(:,:,2) = reshape(interp2(im_warped(:,:,2), v_x(:), v_y(:)), [h, w]);
-% im_final(:,:,3) = reshape(interp2(im_warped(:,:,3), v_x(:), v_y(:)), [h, w]);
-
-% im_rectified = uint8(im_final);
+im_warped = im_warped .* (v_z > 0);
+im_warped = uint8(im_warped);
